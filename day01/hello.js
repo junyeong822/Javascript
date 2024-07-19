aa10c7ce5015bfb74ddbe88462423cff59fb140e
junyeong822 <cyclock12@gmail.com>
2024-07-19 14:55:38 +0900

    Add files via upload

---
+++ day01/hello.js
@@ -0,0 +1,7 @@
+//window.alert("오늘 자바스크립트 배우는 날");
+//window.console.log("주말에는 놀러갈거임")
+
+// window.console.log("주말");
+// window.console.log("저녁 메뉴");
+// window.console.log("자바스크립트 후기");
+
---
+++ day01/memory/index.js
@@ -0,0 +1,23 @@
+const name = "배준영";
+const coffee = "카페라떼";
+
+// window.console.log(name);
+// window.console.log(coffee);
+
+// window.console.log("제 이름은 name 좋아하는 커피는 coffee입니다.");
+// window.console.log(`제 이름은 ${name} 좋아하는 커피는 ${coffee}입니다.`);
+
+// mbti weekend youtube
+// const mbti = "ISFP";
+// const weekend = "주말일정";
+// const youtube = "유튜브";
+
+// window.console.log(`제 mbti는 ${mbti}이구요 주말에는 ${weekend}하구요 유튜브는 ${youtube} 를 볼거에요!`);
+
+// 프롬프트로 당신의 핸드폰 기종은?
+// 프롬프트로 당신의 PC 기종은?
+// 콘솔로 당신이 쓰는 핸드폰은 ~~이고 당신의 PC는 ~~ 이군요!
+
+const phone = window.prompt("당신의 핸드폰 기종은?");
+const pc = window.prompt("당신의 PC 기종은?");
+window.console.log(`당신이 쓰는 핸드폰은 ${phone}이고 당신의 PC는 ${pc}이군요!`);

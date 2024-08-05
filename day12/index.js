ddb4b71538988a18208d6788dd3a5e8a52c58f81
junyeong822 <cyclock12@gmail.com>
2024-08-05 14:46:37 +0900

    Add files via upload

---
+++ day12/index.js
@@ -0,0 +1,324 @@
+const data = [
+  {
+    id: "fkPacELBY",
+    name: "김진영",
+    position: "선생님",
+    gymName: "도화 K-GYM (케이짐 피티스튜디오)",
+    gymAddress: "인천 미추홀구 도화동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/fkPacELBY+2NrXxaX2Ja1EKRY2DG6EM.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "n6PabnNB9",
+    name: "장석우",
+    position: "선생님",
+    gymName: "인하대 피트니스연구소",
+    gymAddress: "인천 미추홀구 용현동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/n6PabnNB9+3rpNZRMbR6ukkRpHStadd.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "2UjnR9L9Cd",
+    name: "임진균",
+    position: "선생님",
+    gymName: "인하대 피트니스연구소",
+    gymAddress: "인천 미추홀구 용현동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/2UjnR9L9Cd+abC1invtV1q.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "zmPw14CUf",
+    name: "김도형",
+    position: "선생님",
+    gymName: "인하대 피트니스연구소",
+    gymAddress: "인천 미추홀구 용현동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/zmPw14CUf+2P8QaxsKNai543jSoyvm8.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "9MXEdQ6aDU",
+    name: "김준호",
+    position: "선생님",
+    gymName: "인천 루트짐 PT",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/9MXEdQ6aDU+2SQ3JehJcx1XyYPnP2sGb.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "3aUQ4CZYAg",
+    name: "한윤지",
+    position: "선생님",
+    gymName: "주안 홍 휘트니스 24시",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/3aUQ4CZYAg+2S5Mg7qkzYVDRNM5ZSSfm.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: false,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "22wNzjFb9P",
+    name: "김주엽",
+    position: "선생님",
+    gymName: "주안 홍 휘트니스 24시",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/22wNzjFb9P+TM8XMM8LtJnW.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: false,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "263ePdHmaj",
+    name: "고상철",
+    position: "선생님",
+    gymName: "주안 홍 휘트니스 24시",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/263ePdHmaj+GR35KaxbtBrM.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: false,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "ALwgoW87RY",
+    name: "구종욱",
+    position: "선생님",
+    gymName: "주안 홍 휘트니스 24시",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/ALwgoW87RY+2SPjtzFM9KyatbR4UW9cH.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "y8HJ4cpLh",
+    name: "황효상",
+    position: "선생님",
+    gymName: "주안 토크짐",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/y8HJ4cpLh+FkEQATZeRqie.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "wicfKLt18",
+    name: "장성호",
+    position: "선생님",
+    gymName: "주안 토크짐",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/wicfKLt18+3j1pEALHRkK85pvGXqLPH.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 4,
+    reviewCount: 2,
+  },
+  {
+    id: "rqGgjLWZN",
+    name: "함유성",
+    position: "선생님",
+    gymName: "주안 토크짐",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/rqGgjLWZN+2NrE8kh985dyuyyEsq2ry.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "2BfKUZmjxH",
+    name: "김대진",
+    position: "선생님",
+    gymName: "주안 터닝포인트짐",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/2BfKUZmjxH+JpsLmdZgbrg9.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: true,
+    rate: 3,
+    reviewCount: 1,
+  },
+  {
+    id: "xZuzX4MeA",
+    name: "김태하",
+    position: "선생님",
+    gymName: "주안 웨이빗 피트니스",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/xZuzX4MeA+EwZmMZREvAkM.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: "8JPhMmGGSC",
+    name: "전승민",
+    position: "선생님",
+    gymName: "주안 웨이빗 피트니스",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/8JPhMmGGSC+2QSW4qaAtmxBxT46Xia3e.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 5,
+    reviewCount: 10,
+  },
+  {
+    id: "6pQ2BZox9g",
+    name: "김은호",
+    position: "선생님",
+    gymName: "주안 웨이빗 피트니스",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/6pQ2BZox9g+2SPfFQyrrGQHp1auF7YQP.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 5,
+    reviewCount: 2,
+  },
+  {
+    id: "2TuynKiECn",
+    name: "박찬환",
+    position: "선생님",
+    gymName: "주안 웨이빗 피트니스",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/2TuynKiECn+dud3T6CECRm9HgfpQEY.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 5,
+    reviewCount: 2,
+  },
+  {
+    id: "jTk38ZpkU",
+    name: "장준영",
+    position: "선생님",
+    gymName: "주안 웨이빗 피트니스",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/jTk38ZpkU+2YaqahAk1H8SnMtPzPSUP.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 5,
+    reviewCount: 1,
+  },
+  {
+    id: "qcZRftBo1",
+    name: "박무성",
+    position: "선생님",
+    gymName: "주안 웨이빗 피트니스",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/qcZRftBo1+2S7VHELUfi82Loqhp4Vv8.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 5,
+    reviewCount: 1,
+  },
+  {
+    id: "wMhhTwkhN",
+    name: "박정민",
+    position: "선생님",
+    gymName: "주안 웨이빗 피트니스",
+    gymAddress: "인천 미추홀구 주안동",
+    profileImage:
+      "https://api.da-gym.net/trainers/images/wMhhTwkhN+KTZxHc2NUuK7DpVXQLC.jpg",
+    exerciseTypes: ["P.T"],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+];
+
+const container = document.createElement("div");
+container.style.display = "grid";
+container.style.gridTemplateColumns = "repeat(2,1fr)";
+
+data.forEach((x) => {
+  const box = document.createElement("div");
+  box.className = "trainer";
+  box.style.boxShadow = `rgba(0, 0, 0, 0.35) 0px 5px 15px`;
+  box.innerHTML = `
+    <div class="imagebox">
+        <img src="${x.profileImage}" alt="">
+      </div>
+      <div class="info">
+        <span>${x.name}</span>
+        <span>${x.gymAddress}</span>
+      </div>
+  `;
+  container.appendChild(box);
+});
+
+document.body.appendChild(container);
